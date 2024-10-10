f9ff3eb7f768095290d7e36c4e4dcf6ffeafe3f3
Gabrielly <gabriellydiaspereira15@gmail.com>
2024-10-10 17:16:39 -0300

    marcação de verificação de email

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
@@ -11,7 +12,18 @@ class WelcomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return WelcomeController::class;
-        //
+        $user = User::find(1);
+
+        // $user = User::query()->create([
+        //     'name' => 'Gabrielly',
+        //     'email' => 'gustavo@gmail.com',
+        //     'password' => 'noturno'
+        // ]);
+
+        $user->email_verified_at = now();
+        $user->save();
+
+        dd($user->email_verified_at);
     }
 }
+

b18735f512456b0729b3db0ca6f31cb1a68a2a43
Gabrielly <gabriellydiaspereira15@gmail.com>
2024-10-24 18:39:27 -0300

    dinâmica de rotas

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,9 @@
+<x-layouts.app>
+
+    <a href='{{ route('projects.show', 1)}}'>
+
+        primeiro projeto
+
+    </a>
+
+</x-layouts.app>
---
+++ resources/views/projects/show.blade.php
@@ -0,0 +1,5 @@
+<x-layouts.app>
+
+    show
+
+</x-layouts.app>
